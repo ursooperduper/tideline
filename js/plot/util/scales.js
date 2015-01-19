5e966f11ccbfb044522bec51e5bd479155f236d7
Sarah Kuehnle <sarah@sooperduper.com>
2015-01-19 17:56:14 -0500

    Updates scales and removes guidelines for the BG chart in the daily view.

--- js/plot/util/scales.js
+++ js/plot/util/scales.js
@@ -1,15 +1,15 @@
 /*
  * == BSD2 LICENSE ==
  * Copyright (c) 2014, Tidepool Project
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the associated License, which is identical to the BSD 2-Clause
  * License as published by the Open Source Initiative at opensource.org.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the License for more details.
- * 
+ *
  * You should have received a copy of the License along with this program; if
  * not, you can obtain one from Tidepool Project at tidepool.org.
  * == BSD2 LICENSE ==
@@ -74,7 +74,7 @@ var scales = function(opts) {
       if ((!data) || (data.length === 0)) {
         return [];
       }
-      var defaultTicks = opts.bgUnits === 'mg/dL' ? [40, 80, 120, 180, 300] : [2.0, 4.5, 7.0, 10.0, 16.0];
+      var defaultTicks = opts.bgUnits === 'mg/dL' ? [80, 180, 300] : [4.5, 10.0, 16.0];
       var ext = d3.extent(data, function(d) { return d.value; });
       if (ext[0] === ext[1]) {
         return defaultTicks;
--- plugins/blip/chartdailyfactory.js
+++ plugins/blip/chartdailyfactory.js
@@ -1,15 +1,15 @@
 /*
  * == BSD2 LICENSE ==
  * Copyright (c) 2014, Tidepool Project
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the associated License, which is identical to the BSD 2-Clause
  * License as published by the Open Source Initiative at opensource.org.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the License for more details.
- * 
+ *
  * You should have received a copy of the License along with this program; if
  * not, you can obtain one from Tidepool Project at tidepool.org.
  * == BSD2 LICENSE ==
@@ -215,16 +215,17 @@ function chartDailyFactory(el, options) {
     // add background fill rectangles to BG pool
     poolBG.addPlotType('fill', fill(poolBG, {
       endpoints: chart.endpoints,
-      guidelines: [
-        {
-          'class': 'd3-line-bg-threshold',
-          'height': chart.options.bgClasses.low.boundary
-        },
-        {
-          'class': 'd3-line-bg-threshold',
-          'height': chart.options.bgClasses.target.boundary
-        }
-      ],
+
+      // guidelines: [
+      //   {
+      //     'class': 'd3-line-bg-threshold',
+      //     'height': chart.options.bgClasses.low.boundary
+      //   },
+      //   {
+      //     'class': 'd3-line-bg-threshold',
+      //     'height': chart.options.bgClasses.target.boundary
+      //   }
+      // ],
       yScale: scaleBG
     }), true, true);
 
